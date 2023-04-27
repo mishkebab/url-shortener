e71b726144200b36be1507380e8f4d5a0e422845
misha <mishabansal7@gmail.com>
2023-04-27 16:12:08 -0700

    created users table and model

---
+++ URLShortener/db/migrate/20230427223854_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string "email", null:false
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.timestamps
+    end
+  end
+end
