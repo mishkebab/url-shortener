2b51f72cc62fd772f87a94cfa7d61260ff95f6f0
misha <mishabansal7@gmail.com>
2023-04-27 17:04:32 -0700

    finished phase 2

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,4 +1,25 @@
 class ShortenedUrl < ApplicationRecord
     validates :short_url, presence: true, uniqueness: true
     validates :long_url, :user_id, presence: true
+
+    after_initialize do |short_url|
+        generate_short_url
+        puts "you have shortened a url"
+    end 
+
+    def self.random_code
+        random_code = SecureRandom.urlsafe_base64
+        while ShortenedUrl.exists?(random_code)
+            random_code = SecureRandom.urlsafe_base64
+        end
+        random_code
+    end 
+
+    private 
+    
+    def generate_short_url
+        if !short_url
+            short_url = ShortenedUrl.random_code
+        end
+    end 
 end
